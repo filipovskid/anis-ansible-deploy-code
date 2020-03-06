c48c9152439b34cb2a989683744337764ff659b8
Darko Filipovski <darkofilipovski0@gmail.com>
2020-03-06 09:20:32 +0100

    Spring Boot backend init

---
+++ drboson-backend/src/main/java/com/filipovski/drboson/drboson/DrbosonApplication.java
@@ -0,0 +1,13 @@
+package com.filipovski.drboson.drboson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrbosonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrbosonApplication.class, args);
+    }
+
+}
---
+++ drboson-backend/src/test/java/com/filipovski/drboson/drboson/DrbosonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipovski.drboson.drboson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrbosonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
