5c8b85bc6a1be5d34f54e584d43e368b7696a7f6
Darko Filipovski <darkofilipovski0@gmail.com>
2020-03-15 17:30:25 +0100

    User registration and login in a single Auth controller

--- drboson-backend/src/main/java/com/filipovski/drboson/drboson/web/controllers/AuthController.java
+++ drboson-backend/src/main/java/com/filipovski/drboson/drboson/web/controllers/AuthController.java
@@ -4,16 +4,19 @@ import com.filipovski.drboson.drboson.model.jwt.AuthenticationRequest;
 import com.filipovski.drboson.drboson.model.jwt.AuthenticationResponse;
 import com.filipovski.drboson.drboson.service.IUserService;
 import com.filipovski.drboson.drboson.service.UserService;
+import com.filipovski.drboson.drboson.service.dtos.UserRegistrationDto;
+import com.filipovski.drboson.drboson.service.exceptions.UsernameAlreadyExistsException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@RequestMapping(path = "/auth", produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
 public class AuthController {
 
     private final IUserService userService;
@@ -22,11 +25,17 @@ public class AuthController {
         this.userService = userService;
     }
 
-    @PostMapping("/auth/login")
+    @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> loginUser(@RequestBody AuthenticationRequest authenticationRequest,
                                                             HttpServletResponse response) {
         AuthenticationResponse authenticationResponse = userService.userLogin(authenticationRequest, response);
 
         return ResponseEntity.ok(authenticationResponse);
+    }
+
+    @PostMapping("/join")
+    @ResponseStatus(HttpStatus.OK)
+    public void registerUser(@RequestBody @Valid UserRegistrationDto user) throws UsernameAlreadyExistsException {
+        userService.registerNewUserAccount(user);
     }
 }
